9e7d924406bcb3e23b4dcde72d79ff884773dbea
GreenNun <stas909@gmail.com>
2019-08-12 22:11:17 +0300

    initial

---
+++ src/Hello.kt
@@ -0,0 +1,11 @@
+/**
+ * Created by GreenNun on 2019-08-12.
+ */
+fun main(args: Array<String>) {
+    println("Hello, world!")
+    val playerName = "Estragon"
+    var experiencePoints = 5
+//    experiencePoints = "thirty-two"
+    experiencePoints = 10
+    println("'$playerName' points: $experiencePoints")
+}

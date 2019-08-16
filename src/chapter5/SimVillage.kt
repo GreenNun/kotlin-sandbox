c483e6e3811ea32f3a0c164461c943c18368e5c0
Stanislav Bausov <s.bausov@cardsmobile.com>
2019-08-16 13:17:52 +0300

    5.4

--- src/chapter5/SimVillage.kt
+++ src/chapter5/SimVillage.kt
@@ -4,9 +4,9 @@ package chapter5
  * Created by Stanislav Bausov on 14.08.2019.
  */
 fun main() {
-    val greetingFunction: (String) -> String = { playerName ->
+    val greetingFunction: (String) -> String = {
         val currentYear = 2019
-        "Welcome to SimVillage, $playerName! (copyright $currentYear)"
+        "Welcome to SimVillage, $it! (copyright $currentYear)"
     }
 
     println(greetingFunction("Guyal"))

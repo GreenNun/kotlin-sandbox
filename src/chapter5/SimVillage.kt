9871b5dc3f82af2f640a0220188f637981f47465
Stanislav Bausov <s.bausov@cardsmobile.com>
2019-08-16 13:20:19 +0300

    5.5

--- src/chapter5/SimVillage.kt
+++ src/chapter5/SimVillage.kt
@@ -4,10 +4,11 @@ package chapter5
  * Created by Stanislav Bausov on 14.08.2019.
  */
 fun main() {
-    val greetingFunction: (String) -> String = {
+    val greetingFunction: (String, Int) -> String = { playerName, numBuildings ->
         val currentYear = 2019
-        "Welcome to SimVillage, $it! (copyright $currentYear)"
+        println("Adding $numBuildings houses")
+        "Welcome to SimVillage, $playerName! (copyright $currentYear)"
     }
 
-    println(greetingFunction("Guyal"))
+    println(greetingFunction("Guyal", 2))
 }
